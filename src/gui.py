c9c1bc616b534d53c2374d0551a763d2f6745887
Chris Sanchez <christian.valenzuela@correounivalle.edu.co>
2023-10-17 00:51:00 -0500

    Setup poetry

---
+++ src/gui.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+
+def on_button_click():
+    if label["text"] == "Presiona el botón:":
+        label["text"] = "¡Hola, mundo!"
+    else:
+        label["text"] = "Presiona el botón:"
+
+
+app = tk.Tk()
+app.geometry("300x200")
+app.title("Ejemplo con Tkinter")
+
+label = tk.Label(app, text="Presiona el botón:")
+label.pack(padx=20, pady=20)
+
+button = tk.Button(app, text="Haz clic", command=on_button_click)
+button.pack()
+
+app.mainloop()
