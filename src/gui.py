6c92bc0e0ab25cd03bb0ef3e13a0cd7e2154b60d
Chris Sanchez <christian.valenzuela@correounivalle.edu.co>
2023-10-18 23:52:57 -0500

    Add table costs

--- src/gui.py
+++ src/gui.py
@@ -1,21 +1,66 @@
-import tkinter as tk
+import tkinter
+from tkinter import *
+
+num_days = 0
+num_clients = 0
+matriz = []
 
 
-def on_button_click():
-    if label["text"] == "Presiona el botón:":
-        label["text"] = "¡Hola, mundo!"
-    else:
-        label["text"] = "Presiona el botón:"
+app = tkinter.Tk()
+app.title("Plantas de Energı́a")
+app.geometry("900x900")
+
+etiqueta = tkinter.Label(app, text="Plantas de Energı́a", font=(
+    'Times New Roman', 15), pady=5, padx=0)
+etiqueta.place(x=150, y=20)
+
+lb_clients = Label(app, text="Ingrese el número de clientes =>")
+lb_clients.place(x=20, y=80)
+
+lb_days = Label(app, text="Ingrese el número de días =>")
+lb_days.place(x=20, y=120)
+
+input_clients = Entry(app, width=10)
+input_clients.place(x=230, y=80)
+
+input_days = Entry(app, width=10)
+input_days.place(x=230, y=120)
+
+matriz
 
 
-app = tk.Tk()
-app.geometry("700x700")
-app.title("Ejemplo con Tkinter")
+def init_Tb_paymenst(num_clients: int):
+    columnas = []
+    for i in range(2):
+        filas = []
+        for j in range(num_clients):
+            e = Entry(app, relief=RIDGE)
+            if i == 0:
+                e.insert(END, f'Cliente {j+1}')
+            e.place(x=40+i*160, y=215+j*20)
+            filas.append(e)
+        columnas.append(filas)
 
-label = tk.Label(app, text="Presiona el botón:")
-label.pack(padx=20, pady=20)
+    matriz.append(columnas)
 
-button = tk.Button(app, text="Haz clic", command=on_button_click)
-button.pack()
+
+def clickStart():
+    global num_clients
+
+    try:
+        num_clients = int(input_clients.get(), 10)
+        init_Tb_paymenst(num_clients)
+    except:
+        IndexError
+
+
+boton_cargar = Button(
+    app, text="Iniciar carga de datos", command=clickStart, width=25)
+boton_cargar.place(x=350, y=80)
+
+label_topic = Label(app, text="Clientes", font=('Times New Roman', 12))
+label_topic.place(x=65, y=190)
+label_min = Label(app, text="Pago", font=('Times New Roman', 12))
+label_min.place(x=220, y=190)
 
 app.mainloop()
